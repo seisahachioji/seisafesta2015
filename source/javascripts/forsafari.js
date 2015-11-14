ac7500ecd2f532368f7b409dca1785095bd5e3f6
Shuma <s64.stdio@gmail.com>
2015-11-14 22:17:36 +0900

    fix ios7 vh bug

--- source/javascripts/forsafari.js
+++ source/javascripts/forsafari.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
   if(navigator.userAgent.match(/Safari/i) && !navigator.userAgent.match(/Chrome/i)) {
-    $('body').addClass('onsafari')
+    $('body').addClass('onsafari');
+  }
+
+  if( navigator.userAgent.match(/like Mac OS X/i) ) {
+    $('body').addClass('onios');
+  }
+
+  if( navigator.userAgent.match(/OS 7_.+? like Mac OS X/i) ) {
+    $('body').addClass('onios7');
   }
 
 });
